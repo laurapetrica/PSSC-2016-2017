a1dcae2df952e148b924d84fb66cb31ed75f7eb6
Vali <vali.boar@yahoo.com>
2016-10-26 21:27:40 +0300

    [VB] Add Student related classes

---
+++ PSSC/Models/StudentModel/Subject.cs
@@ -0,0 +1,105 @@
+﻿using Models.ProfessorModel;
+using Models.UniversityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.StudentModel
+{
+    enum AssessmentType
+    {
+        Exam,
+        Distributed
+    };
+
+    class Subject
+    {
+        private string name;
+
+        private byte creditsNo;
+
+        private AssessmentType assessmentType;
+
+        private double activityProportion;
+
+        private StudyYearValue studyYear;
+
+        public Subject(string name, byte creditsNo, AssessmentType assessmentType, double activityProportion, StudyYearValue studyYear)
+        {
+            this.name = name;
+            this.creditsNo = creditsNo;
+            this.assessmentType = assessmentType;
+            this.activityProportion = activityProportion;
+            this.studyYear = studyYear;
+        }
+
+        #region Getters and Setters
+        public double ActivityProportion
+        {
+            get
+            {
+                return activityProportion;
+            }
+
+            set
+            {
+                activityProportion = value;
+            }
+        }
+
+        internal AssessmentType AssessmentType
+        {
+            get
+            {
+                return assessmentType;
+            }
+
+            set
+            {
+                assessmentType = value;
+            }
+        }
+
+        public byte CreditsNo
+        {
+            get
+            {
+                return creditsNo;
+            }
+
+            set
+            {
+                creditsNo = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        internal StudyYearValue StudyYear
+        {
+            get
+            {
+                return studyYear;
+            }
+
+            set
+            {
+                studyYear = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ PSSC/Models/StudentModel/SubjectsPerYear.cs
@@ -0,0 +1,67 @@
+﻿using Models.StudentModel.Behavior;
+using Models.UniversityModel;
+using Models.UniversityModel.Behavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.ProfessorModel;
+
+namespace Models.StudentModel
+{
+
+    enum StudyYearValue
+    {
+        I,
+        II,
+        III,
+        IV
+    };
+
+    class SubjectsPerYear
+    {
+        private StudyYearValue studyYear;
+
+        private HashSet<Subject> allocatedSubjects;
+
+        public SubjectsPerYear()
+        {
+            allocatedSubjects = new HashSet<Subject>();
+        }
+
+        public SubjectsPerYear(StudyYearValue studyYear, HashSet<Subject> allocatedSubjects)
+        {
+            this.studyYear = studyYear;
+            this.allocatedSubjects = allocatedSubjects;
+        }
+
+        #region Getters and Setters
+        internal StudyYearValue StudyYear
+        {
+            get
+            {
+                return studyYear;
+            }
+
+            set
+            {
+                studyYear = value;
+            }
+        }
+
+        internal HashSet<Subject> AllocatedSubjects
+        {
+            get
+            {
+                return allocatedSubjects;
+            }
+
+            set
+            {
+                allocatedSubjects = value;
+            }
+        }
+        #endregion
+    }
+}
